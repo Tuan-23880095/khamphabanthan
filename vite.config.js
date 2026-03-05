d095904416daffe24d7ef109111746a107e51514
Đinh Quốc Tuấn <dqtuanhcmus@gmail.com>
2026-03-05 11:07:48 +0700

    Set base path for Vite deployment
    Add base path for deployment in Vite config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/khamphabanthan/', // <--- Phải thêm dòng này, tên đúng bằng tên kho lưu trữ của bạn
 })
